874d01d4aed9a27fcf17ea1fc8dfd8f17c56acad
Manuel Pancorbo <manuel.pancorbo@scmspain.com>
2018-02-06 18:45:51 +0100

    Test decreasing of quality after a day when item sell in date has passed

--- src/test/java/com/milanuncios/codingdojo/gildedrose/GildedRoseShould.java
+++ src/test/java/com/milanuncios/codingdojo/gildedrose/GildedRoseShould.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertEquals;
 public class GildedRoseShould {
   @Test
   public void decrease_sell_in_after_every_day() {
-    Item regularItem = new Item("any name", 5, 5);
-    GildedRose gildedRose = new GildedRose(new Item[]{regularItem});
+    Item item = new Item("any name", 5, 5);
+    GildedRose gildedRose = new GildedRose(new Item[]{item});
 
     gildedRose.updateQuality();
 
@@ -16,12 +16,22 @@ public class GildedRoseShould {
   }
 
   @Test
-  public void decrease_quality_after_every_day() {
-    Item regularItem = new Item("any name", 5, 5);
-    GildedRose gildedRose = new GildedRose(new Item[]{regularItem});
+  public void degrade_quality_after_every_day() {
+    Item item = new Item("any name", 5, 5);
+    GildedRose gildedRose = new GildedRose(new Item[]{item});
 
     gildedRose.updateQuality();
 
     assertEquals(4, gildedRose.items[0].quality);
+  }
+
+  @Test
+  public void degrade_twice_faster_after_item_sell_in_date_has_passed() {
+    Item item = new Item("any name", 0, 5);
+    GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+    gildedRose.updateQuality();
+
+    assertEquals(3, gildedRose.items[0].quality);
   }
 }
