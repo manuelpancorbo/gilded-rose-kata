b1991209dd882e8d1fc32159b94ab5038ff9500b
Manuel Pancorbo <manuel.pancorbo@scmspain.com>
2018-02-06 18:48:55 +0100

    Test increasing of quality after a day when item is aged brie

--- src/test/java/com/milanuncios/codingdojo/gildedrose/GildedRoseShould.java
+++ src/test/java/com/milanuncios/codingdojo/gildedrose/GildedRoseShould.java
@@ -27,11 +27,21 @@ public class GildedRoseShould {
 
   @Test
   public void degrade_twice_faster_after_item_sell_in_date_has_passed() {
-    Item item = new Item("any name", 0, 5);
+    Item item = new Item("Aged Brie", 5, 5);
     GildedRose gildedRose = new GildedRose(new Item[]{item});
 
     gildedRose.updateQuality();
 
-    assertEquals(3, gildedRose.items[0].quality);
+    assertEquals(6, gildedRose.items[0].quality);
+  }
+
+  @Test
+  public void increase_aged_brie_quality_after_every_day() {
+    Item agedBrie = new Item("Aged Brie", 5, 5);
+    GildedRose gildedRose = new GildedRose(new Item[]{agedBrie});
+
+    gildedRose.updateQuality();
+
+    assertEquals(6, gildedRose.items[0].quality);
   }
 }
