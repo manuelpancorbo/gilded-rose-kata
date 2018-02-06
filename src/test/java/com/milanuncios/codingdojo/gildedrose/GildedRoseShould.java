ccad9b38fc1617f508ef2edb616ac194b74654bc
Manuel Pancorbo <manuel.pancorbo@scmspain.com>
2018-02-06 18:38:42 +0100

    Test decreasing of selling after a day

---
+++ src/test/java/com/milanuncios/codingdojo/gildedrose/GildedRoseShould.java
@@ -0,0 +1,17 @@
+package com.milanuncios.codingdojo.gildedrose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GildedRoseShould {
+  @Test
+  public void decrease_sell_in_after_every_day() {
+    Item regularItem = new Item("any name", 5, 5);
+    GildedRose gildedRose = new GildedRose(new Item[]{regularItem});
+
+    gildedRose.updateQuality();
+
+    assertEquals(4, gildedRose.items[0].sellIn);
+  }
+}
